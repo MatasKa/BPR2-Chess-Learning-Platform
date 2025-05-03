e10262dd3ee5701c47adc3e4915d15ea2e0953e9
MatasKa <matas.k2002@gmail.com>
2025-05-03 20:59:32 +0200

    The king can mark its legal moves
    can still enter a square that is currently a possible move by the enemy, as there are no checks as of yet.

---
+++ Assets/Scripts/King.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem.iOS;
+
+public class King : Piece
+{
+    public override void PossibleMoves()
+    {
+        base.PossibleMoves();
+        Vector2Int[] directions = new Vector2Int[]
+        {
+        new Vector2Int(+0, +1),
+        new Vector2Int(+1, +1),
+        new Vector2Int(+1, 0),
+        new Vector2Int(+1, -1),
+        new Vector2Int(0, -1),
+        new Vector2Int(-1, -1),
+        new Vector2Int(-1, 0),
+        new Vector2Int(-1, +1)
+        };
+
+        //Add more checks that don't allow to enter a square that is "possible" by opponent
+
+        foreach (Vector2Int dir in directions)
+        {
+            Vector2Int newPos = base.currentSquare + dir;
+            if (board.IsInsideBoard(newPos))
+            {
+                Piece temp = null;
+                temp = board.IsSquareFree(newPos);
+                if (temp == null)
+                {
+                    board.Highlight(newPos);
+                }
+                else if (board.IsEnemyPiece(temp) == true)
+                {
+                    board.Highlight(newPos);
+                }
+            }
+        }
+    }
+}
